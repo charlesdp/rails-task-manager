0f389755de73c4c16d24ee44bc18d10c2f237751
Charles <c.depoortere28@gmail.com>
2016-08-09 16:54:14 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  root to: "tasks#index"
+  # get '/tasks', to: 'tasks#index'
+  # get 'new', to: 'tasks#new'
+  # get 'show', to: 'tasks#show'
 end
